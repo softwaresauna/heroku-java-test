97a81cf45d10d036e4046bf74f30885a233c5177
mbjelac <marko.bjelac@softwaresauna.com>
2020-01-13 13:54:53 +0100

    Added JUnit 5 & a passing dummy test.

---
+++ src/test/java/com/example/DummyTest.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class DummyTest {
+
+    @Test
+    public void dummy() {
+//        Assertions.fail("Oops!");
+    }
+}
